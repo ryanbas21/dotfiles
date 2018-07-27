5158a8727aa4f2152d15dba5cf131b45ca212578
ryanbas21 <ryanbas21@gmail.com>
2018-07-27 12:50:10 -0400

    feat(install.sh) Adding install file - WIP
    Still have to test / work on it.

---
+++ install/brew.sh
@@ -0,0 +1,65 @@
+f test ! "$( which brew )"; then
+    echo "Installing homebrew"
+    ruby -e "$( curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install )"
+fi
+
+echo -e "\\n\\nInstalling homebrew packages..."
+echo "=============================="
+
+formulas=(
+    ack
+    diff-so-fancy
+    direnv
+    dnsmasq
+    git
+    'grep --with-default-names'
+    highlight
+    hub
+    markdown
+    mas
+    neovim/neovim/neovim
+    node
+    nginx
+    python
+    rbenv
+    reattach-to-user-namespace
+    shellcheck
+    trash
+    tree
+    wget
+    vim
+    z
+    zsh
+    ripgrep
+    git-standup
+    entr
+    zplug
+  )
+
+
+for formula in "${formulas[@]}"; do
+    formula_name=$( echo "$formula" | awk '{print $1}' )
+    if brew list "$formula_name" > /dev/null 2>&1; then
+        echo "$formula_name already installed... skipping."
+    else
+        brew install "$formula"
+    fi
+done
+
+# after hte install, install neovim python libraries
+echo -e "\\n\\nRunning Neovim Python install"
+echo "=============================="
+pip2 install --user neovim
+pip3 install --user neovim
+
+# Change the default shell to zsh
+zsh_path="$( which zsh )"
+if ! grep "$zsh_path" /etc/shells; then
+    echo "adding $zsh_path to /etc/shells"
+    echo "$zsh_path" | sudo tee -a /etc/shells
+fi
+
+if [[ "$SHELL" != "$zsh_path" ]]; then
+    chsh -s "$zsh_path"
+    echo "default shell changed to $zsh_path"
+fi
