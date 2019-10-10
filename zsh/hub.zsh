3f172beee81415b20d400a487531c1145638aea2
ryanbas21 <ryanbas21@gmail.com>
2019-10-09 22:47:47 -0500

    touchups

---
+++ zsh/hub.zsh
@@ -0,0 +1,6 @@
+alias showpr= 'hub list pr'
+alias openpr= 'hub pull-request'
+alias gitsync= 'hub sync'
+alias showpr= 'hub browse -- pr'
+alias showissues= 'hub browse -- issues'
+echo source ~/.bash_profile >>  ~/.zshrc
