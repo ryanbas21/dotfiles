c7528ab5fe72252494818822bf0b6245cd0a29ab
Ryan Bas <ryan.basmajian@forgerock.com>
2021-08-25 09:48:22 -0600

    update

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,9 +1,9 @@
 require 'treesitter'
+require'nvimtree'
 require'bufferline'.setup{
    diagnostics = "nvim_lsp"
   }
 require 'telescope_config'
--- require '_compe'
 require '_devicons'
 require '_diffview'
 require'_galaxy_line'
@@ -20,42 +20,6 @@ require 'nvim-autopairs'.setup({ map_cr = true, map_complete = true })
         c = {description = {"  Recents                   Ctrl P"}, command = "Telescope frecency"},
         d = {description = {"  Find Word                 SPC s"}, command = "Telescope live_grep"},
         e = {description = {"  Find In Buffer            SPC ;"}, command = "Telescope current_buffer_fuzzy_find"}
-    }
-
-local tree_cb = require'nvim-tree.config'.nvim_tree_callback
-vim.g.nvim_tree_bindings = {
-      { key = {"<CR>", "o", "<2-LeftMouse>"}, cb = tree_cb("edit") },
-      { key = {"<2-RightMouse>", "<C-]>"},    cb = tree_cb("cd") },
-      { key = "<v>",                        cb = tree_cb("vsplit") },
-      { key = "<x>",                        cb = tree_cb("split") },
-      { key = "<t>",                        cb = tree_cb("tabnew") },
-      { key = "<",                            cb = tree_cb("prev_sibling") },
-      { key = ">",                            cb = tree_cb("next_sibling") },
-      { key = "P",                            cb = tree_cb("parent_node") },
-      { key = "<BS>",                         cb = tree_cb("close_node") },
-      { key = "<S-CR>",                       cb = tree_cb("close_node") },
-      { key = "<Tab>",                        cb = tree_cb("preview") },
-      { key = "K",                            cb = tree_cb("first_sibling") },
-      { key = "J",                            cb = tree_cb("last_sibling") },
-      { key = "I",                            cb = tree_cb("toggle_ignored") },
-      { key = "H",                            cb = tree_cb("toggle_dotfiles") },
-      { key = "R",                            cb = tree_cb("refresh") },
-      { key = "a",                            cb = tree_cb("create") },
-      { key = "d",                            cb = tree_cb("remove") },
-      { key = "r",                            cb = tree_cb("rename") },
-      { key = "<C-r>",                        cb = tree_cb("full_rename") },
-      { key = "x",                            cb = tree_cb("cut") },
-      { key = "c",                            cb = tree_cb("copy") },
-      { key = "p",                            cb = tree_cb("paste") },
-      { key = "y",                            cb = tree_cb("copy_name") },
-      { key = "Y",                            cb = tree_cb("copy_path") },
-      { key = "gy",                           cb = tree_cb("copy_absolute_path") },
-      { key = "[c",                           cb = tree_cb("prev_git_item") },
-      { key = "]c",                           cb = tree_cb("next_git_item") },
-      { key = "-",                            cb = tree_cb("dir_up") },
-      { key = "s",                            cb = tree_cb("vsplit") },
-      { key = "q",                            cb = tree_cb("close") },
-      { key = "g?",                           cb = tree_cb("toggle_help") },
     }
 
 -- require('nightfox').set()
