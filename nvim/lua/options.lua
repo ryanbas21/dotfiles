be11cdf21c985a67fa9ec37bf682c358625b0fb1
ryan <ryanbas21@gmail.com>
2024-09-29 10:52:46 -0600

    update

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -3,7 +3,8 @@ require "nvchad.options"
 -- add yours here!
 
 local o = vim.o
-o.cursorlineopt ='both' -- to enable cursorline!
+vim.opt.swapfile = false
+o.cursorlineopt = "both" -- to enable cursorline!
 
 vim.opt.number = true
 vim.opt.relativenumber = true
