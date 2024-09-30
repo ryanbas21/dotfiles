37cb34f6eb3c14a6e0ba53c795fbde899b9159fe
ryan <ryanbas21@gmail.com>
2024-09-29 20:57:46 -0600

    remove plugins

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,81 +1,7 @@
 return {
-  -- {
-  --   "trunk-io/neovim-trunk",
-  --   lazy = false,
-  --   -- optionally pin a version
-  --   tag = "v0.1.0",
-  --   -- these are optional config arguments (defaults shown)
-  --   config = {
-  --     -- trunkPath = "trunk",
-  --     -- lspArgs = {},
-  --     formatOnSave = true,
-  --     -- formatOnSaveTimeout = 10, -- seconds
-  --     -- logLevel = "info"
-  --   },
-  --   main = "trunk",
-  --   dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-  -- },
   { "dmmulroy/ts-error-translator.nvim" },
-  {
-    "chrisgrieser/nvim-rulebook",
-    keys = {
-      {
-        "<leader>ri",
-        function()
-          require("rulebook").ignoreRule()
-        end,
-      },
-      {
-        "<leader>rl",
-        function()
-          require("rulebook").lookupRule()
-        end,
-      },
-      {
-        "<leader>ry",
-        function()
-          require("rulebook").yankDiagnosticCode()
-        end,
-      },
-      {
-        "<leader>sf",
-        function()
-          require("rulebook").suppressFormatter()
-        end,
-        mode = { "n", "x" },
-      },
-    },
-  },
   { "folke/neodev.nvim" },
-  { "nvim-focus/focus.nvim", version = "*" },
   "marilari88/twoslash-queries.nvim",
-  {
-    "tris203/precognition.nvim",
-    event = "VeryLazy",
-    opts = {
-      startVisible = true,
-      showBlankVirtLine = true,
-      highlightColor = { link = "Comment" },
-      hints = {
-        Caret = { text = "^", prio = 2 },
-        Dollar = { text = "$", prio = 1 },
-        MatchingPair = { text = "%", prio = 5 },
-        Zero = { text = "0", prio = 1 },
-        w = { text = "w", prio = 10 },
-        b = { text = "b", prio = 9 },
-        e = { text = "e", prio = 8 },
-        W = { text = "W", prio = 7 },
-        B = { text = "B", prio = 6 },
-        E = { text = "E", prio = 5 },
-      },
-      gutterHints = {
-        G = { text = "G", prio = 10 },
-        gg = { text = "gg", prio = 9 },
-        PrevParagraph = { text = "{", prio = 8 },
-        NextParagraph = { text = "}", prio = 8 },
-      },
-    },
-  },
   {
     "stevearc/conform.nvim",
     event = "BufWritePre", -- uncomment for format on save
@@ -174,7 +100,7 @@ return {
 
       -- Only one of these is needed, not both.
       "nvim-telescope/telescope.nvim", -- optional
-      "ibhagwan/fzf-lua", -- optional
+      -- "ibhagwan/fzf-lua", -- optional
     },
     config = true,
   },
@@ -203,14 +129,6 @@ return {
     },
   },
   { "VidocqH/lsp-lens.nvim", event = "LspAttach", lazy = true },
-  {
-    "j-hui/fidget.nvim",
-    opts = {
-      ["nvim-tree"] = {
-        enable = true, -- Integrate with nvim-tree/nvim-tree.lua (if installed)
-      },
-    },
-  },
   "b0o/schemastore.nvim",
   {
     "LukasPietzschmann/boo.nvim",
