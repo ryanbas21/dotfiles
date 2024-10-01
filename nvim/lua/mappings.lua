901812c22321ee36c850c513408997374fcc1dd1
ryan <ryanbas21@gmail.com>
2024-10-01 08:36:40 -0600

    update

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -2,8 +2,6 @@ require "nvchad.mappings"
 local neogit = require "neogit"
 local neotest = require "neotest"
 
--- add yours here
-
 local g = vim.g
 local map = vim.keymap.set
 
@@ -14,11 +12,6 @@ map("n", ",,", "<C-6>")
 map("n", "<Leader>;", "<cmd>Telescope live_grep<CR>")
 map("n", "<leader>gs", neogit.open, { noremap = true })
 
-map("n", "K", require("hover").hover, { desc = "hover.nvim" })
-map("n", "gK", require("hover").hover_select, { desc = "hover.nvim (select)" })
-
--- Mouse support
-map("n", "<MouseMove>", require("hover").hover_mouse, { desc = "hover.nvim (mouse)" })
 vim.o.mousemoveevent = true
 
 local mappings = {
