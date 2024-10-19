63a254b7606bf995b721c0548f4c1d9e8bb8adc0
ryan <ryanbas21@gmail.com>
2024-10-19 17:13:29 -0600

    update

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -6,7 +6,7 @@
 local M = {}
 
 M.base46 = {
-  theme = "ayu_dark",
+  theme = "github_dark",
   transparency = false,
 }
 
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,12 +1,5 @@
 return {
   "marilari88/twoslash-queries.nvim",
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require("nvchad.configs.lspconfig").defaults()
-      require "configs.lspconfig"
-    end,
-  },
   { "tpope/vim-repeat", lazy = true, event = "BufEnter" },
   { "tpope/vim-surround", lazy = true, event = "BufEnter" },
   "b0o/schemastore.nvim",
