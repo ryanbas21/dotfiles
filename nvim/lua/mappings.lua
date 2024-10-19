266bcdc8bc407c4edf3a3b951021ab649f70b2b2
ryan <ryanbas21@gmail.com>
2024-10-19 15:35:30 -0600

    update

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,6 +1,5 @@
 require "nvchad.mappings"
 local neogit = require "neogit"
-local neotest = require "neotest"
 
 local g = vim.g
 local map = vim.keymap.set
@@ -11,54 +10,4 @@ map("i", "jk", "<ESC>")
 map("n", ",,", "<C-6>")
 map("n", "<Leader>;", "<cmd>Telescope live_grep<CR>")
 map("n", "<leader>gs", neogit.open, { noremap = true })
-
--- local mappings = {
---   ["<leader>nr"] = function()
---     neotest.run.run(vim.fn.expand "%")
---   end,
---   ["<leader>ns"] = function()
---     for _, adapter_id in ipairs(neotest.run.adapters()) do
---       neotest.run.run { suite = true, adapter = adapter_id }
---     end
---   end,
---   ["<leader>nw"] = function()
---     neotest.watch.watch()
---   end,
---   ["<leader>nx"] = function()
---     neotest.run.stop()
---   end,
---   ["<leader>nn"] = function()
---     neotest.run.run()
---   end,
---   ["<leader>nl"] = function()
---     neotest.run.run_last()
---   end,
---   ["<leader>nD"] = function()
---     neotest.run.run_last { strategy = "dap" }
---   end,
---   ["<leader>na"] = function()
---     neotest.run.attach()
---   end,
---   ["<leader>no"] = function()
---     neotest.output.open { enter = true }
---   end,
---   ["<leader>nO"] = function()
---     neotest.output.open { enter = true, short = true }
---   end,
---   ["<leader>np"] = function()
---     neotest.summary.toggle()
---   end,
---   ["<leader>nm"] = function()
---     neotest.summary.run_marked()
---   end,
---   ["[n"] = function()
---     neotest.jump.prev { status = "failed" }
---   end,
---   ["]n"] = function()
---     neotest.jump.next { status = "failed" }
---   end,
--- }
---
--- for keys, mapping in pairs(mappings) do
---   map("n", keys, "", { callback = mapping, noremap = true })
--- end
+map("n", "<leader>rn", ":IncRename ", { noremap = true })
---
+++ nvim/lua/plugins/hover.lua
@@ -0,0 +1,53 @@
+return {
+  "lewis6991/hover.nvim",
+  config = function()
+    require("hover").setup {
+      init = function()
+        -- Require providers
+        require "hover.providers.lsp"
+        require "hover.providers.gh"
+        require "hover.providers.gh_user"
+        require "hover.providers.jira"
+        -- require "hover.providers.dap"
+        require "hover.providers.fold_preview"
+        require "hover.providers.diagnostic"
+        require "hover.providers.man"
+        require "hover.providers.dictionary"
+      end,
+      preview_opts = {
+        border = "single",
+      },
+      -- Whether the contents of a currently open hover window should be moved
+      -- to a :h preview-window when pressing the hover keymap.
+      preview_window = true,
+      title = true,
+      mouse_providers = {
+        "LSP",
+      },
+      mouse_delay = 1000,
+    }
+  end,
+  keys = {
+    {
+      "K",
+      function()
+        require("hover").hover()
+      end,
+      { desc = "hover.nvim" },
+    },
+    {
+      "gK",
+      function(a)
+        require("hover").hover_select(a)
+      end,
+      { desc = "hover.nvim" },
+    },
+  },
+}
+-- vim.keymap.set("n", "<C-p>", function() require("hover").hover_switch("previous") end, {desc = "hover.nvim (previous source)"})
+-- vim.keymap.set("n", "<C-n>", function() require("hover").hover_switch("next") end, {desc = "hover.nvim (next source)"})
+--
+-- -- Mouse support
+-- vim.keymap.set('n', '<MouseMove>', require('hover').hover_mouse, { desc = "hover.nvim (mouse)" })
+
+-- vim.o.mousemoveevent = true
